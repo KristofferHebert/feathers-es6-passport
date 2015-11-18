08da470b89759f2ec4409a9b5ccdbb7856bf1e1d
= <kristoffer@kristofferhebert.com>
2015-11-17 18:16:19 -0800

    adding comments

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 'use strict'
-
+// Require modules
 let feathers = require('feathers')
+
+// Instantiate server
 let app = feathers()
 
 // Set Default Port
@@ -12,6 +14,7 @@ require('./config')(app)
 // Configure API
 require('../server/api/routes')(app)
 
+// Launch Server
 app.info('Server listening on', PORT)
 
 app.listen(PORT)

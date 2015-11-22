55c2e00b2c0f9518026315cb01c2b55f93503196
= <kristoffer@kristofferhebert.com>
2015-11-21 21:04:16 -0800

    fixed white space

--- server/config/localsSample.js
+++ server/config/localsSample.js
@@ -1,4 +1,5 @@
 // Local settings like api keys and secrets go here. Add your settings here. Rename to locals.js
+
 module.exports = {
     secret: "YOUR SECRET HERE",
     adminEmail: "test@testemail.com",

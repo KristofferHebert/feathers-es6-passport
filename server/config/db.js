5ecc894bfcf1ed8b16d5e5a4ee8320fac0111314
= <kristoffer@kristofferhebert.com>
2015-11-22 22:55:03 -0800

    added comments to db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 'use strict'
 
+// use mongoose for db orm
 let db = require('feathers-mongoose')
 // let db = require('feathers-memory')
 // let db = require('feathers-postgresql')
 
 let LOCALS = require('./locals')
 
+// Configure db based on enviroment
 let dbName = (process.env.NODE_ENV === "production") ? LOCALS.productionDB : LOCALS.stagingDB
 
 console.log('Using DB ', dbName)

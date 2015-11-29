ca3cf03c43dcf9f34aefcb3206b217f0c1badf98
= <kristoffer@kristofferhebert.com>
2015-11-28 18:20:17 -0800

    started troubleshooting api

--- server/api/services/user.js
+++ server/api/services/user.js
@@ -1,15 +1,17 @@
 'use strict'
 
 let UserModel = require('../model/user')
-let feathersAuth = require('feathers-authentication')
-let requireAuth = feathersAuth.hooks.requireAuth
-let hashPassword = feathersAuth.hooks.hashPassword
-let lowercaseEmail = feathersAuth.hooks.lowercaseEmail
+let feathersAuth = require('feathers-authentication').default
+let authHooks = require('feathers-authentication').hooks
 let LOCALS = require('../../config/locals')
 
 module.exports = function(app, path){
 
         app.use(path, UserModel())
+        .use('/user/:id', function (req, res, next) {
+            console.log('Request', req);
+            next();
+        })
         // .use(function(err, req, res, next) {
         //     res.status(500)
         //     res.json({
@@ -19,18 +21,17 @@ module.exports = function(app, path){
 
     	let UserService = app.service(path)
 
-        UserService.before({
-    		all: requireAuth,
-    		create: [hashPassword, lowercaseEmail]
-    	})
+        // UserService.before({
+    	// 	create: [authHooks.hashPassword, authHooks.lowercaseEmail]
+    	// })
 
     	// Create Power User
-    	// userService.create({
-    	// 	email: LOCALS.adminEmail,
-    	// 	password: LOCALS.adminPassword,
-    	// 	admin : true,
-    	// }, {}, function(error, user) {
-    	//  if(error) console.log(error)
-    	// 	console.log('Created Power User', user);
-    	// });
+    	UserService.create({
+    		email: LOCALS.adminEmail,
+    		password: LOCALS.adminPassword,
+    		admin : true,
+    	}, {}, function(error, user) {
+    	 if(error) console.log(error)
+    		console.log('Created Power User', user);
+    	});
 }
